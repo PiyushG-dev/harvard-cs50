4bf8fc56a5c44c211911e494fef20d3cff02cc52
Piyush Goswami <pjgpiyush@gmail.com>
2024-04-19 22:36:28 +0530

    lecture 3 end

---
+++ 03_lecture/iterate.c
@@ -0,0 +1,20 @@
+// cc iterate.c -o iterate cs50.c
+
+#include <cs50.h>
+#include <stdio.h>
+
+void draw(int n);
+int main (void)
+{
+ int height = get_int("What's the height?");
+ draw(height);
+}
+
+void draw (int n) {
+   for(int i = 0; i<n; i++){
+      for(int j = 0; j<i+1; j++){
+        printf("#");
+      }
+      printf("\n");
+   }
+}
