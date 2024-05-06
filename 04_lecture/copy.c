537146da7abc45fa3699a423132815f65db592e2
Piyush Goswami <pjgpiyush@gmail.com>
2024-05-06 22:19:13 +0530

    extras

--- 04_lecture/copy.c
+++ 04_lecture/copy.c
@@ -3,10 +3,15 @@
 #include <cs50.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdlib.h>
 
 int main (void){
     string s = get_string("s: ");
-    string t = s;
+    string t = malloc(strlen(s) + 1);
+
+    for(int i = 0; i<strlen(s) + 1; i++){
+        t[i] = s[i];
+    }
     
     if(strlen(t)> 0) {
         t[0] = toupper(t[0]);
